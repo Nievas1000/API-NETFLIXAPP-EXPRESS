9d3bf3bf928f4b199c0fcc1e98c86588602e9b3e
Lautaro Nievas <lautynievas09@gmail.com>
2023-06-07 21:02:06 -0300

    get videos added

--- src/routes/moviesRoute.js
+++ src/routes/moviesRoute.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const moviesController = require("../movies/controllers/moviesController");
 
 // Movies endpoints
-router.route("/movie/:id").get(moviesController.getMovieById);
 router.route("/movie/popular").get(moviesController.getPopularMovies);
 router.route("/movie/topRated").get(moviesController.getTopRatedMovies);
 router.route("/movie/upcoming").get(moviesController.getUpcomingMovies);
 router.route("/movies/genres").get(moviesController.getGenres);
+router.route("/movie/videos/:id").get(moviesController.getVideosByMovie);
 router.route("/movies/genre/:id").get(moviesController.getMoviesByGenre);
 router.route("/movie/similar/:id").get(moviesController.getSimilarMovies);
 router.route("/movie/search").get(moviesController.getMoviesByKeyword);
 router.route("/trending").get(moviesController.getTrending);
+router.route("/movie/:id").get(moviesController.getMovieById);
 
 module.exports = router;
--- src/routes/seriesRoute.js
+++ src/routes/seriesRoute.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const seriesController = require("../movies/controllers/seriesController");
 
-router.route("/tv/serie/:id").get(seriesController.getSerieById);
 router.route("/tv/popular").get(seriesController.getPopularSeries);
 router.route("/tv/topRated").get(seriesController.getTopRatedSeries);
 router.route("/tv/genres").get(seriesController.getGenres);
+router.route("/tv/videos/:id").get(seriesController.getVideosBySeries);
 router.route("/tv/genre/:id").get(seriesController.getSeriesByGenre);
 router.route("/tv/similar/:id").get(seriesController.getSimilarSeries);
 router.route("/tv/search").get(seriesController.getSeriesByKeyword);
+router.route("/tv/:id").get(seriesController.getSerieById);
 
 module.exports = router;
